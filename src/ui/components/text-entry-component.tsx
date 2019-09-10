756e03c2bbf2a8c57dcbabe0ba2764a75b5f1c65
Aaron Freytag <aaroncfreytag@gmail.com>
2019-09-10 12:22:25 -0700

    Retooled text entry for future use with editable text displays

--- src/ui/components/text-entry-component.tsx
+++ src/ui/components/text-entry-component.tsx
@@ -4,7 +4,9 @@ interface ITextEntryProperties {
     class: string;
     value: string;
     onChange: (newValue: string) => void;
-    onEnterKey: (msg: string) => void;
+    onEntry: (msg: string) => void;
+    pretty?: boolean;
+    enterOnUnfocus?: boolean;
 }
 
 export default class TextEntryComponent extends React.Component<ITextEntryProperties> {
@@ -16,13 +18,18 @@ export default class TextEntryComponent extends React.Component<ITextEntryProper
             // TODO: Holding shift shouldn't trigger this
             // And instead put a line break
             if (event.keyCode === 13) {
-                this.props.onEnterKey(this.props.value);
+                this.props.onEntry(this.props.value);
             }
         };
         return <input type="text"
-            className={`text-entry ${this.props.class}`}
+            className={`text-entry ${this.props.class} ${this.props.pretty ? "pretty-text-entry" : ""}`}
             onKeyDown={callback}
             onChange={(event: React.ChangeEvent<HTMLElement>) => this.props.onChange((event.target as any).value)}
+            onBlur={(event: React.FocusEvent<HTMLElement>) => {
+                if (this.props.enterOnUnfocus) {
+                    this.props.onEntry(this.props.value);
+                }
+            }}
             value={this.props.value}
         />;
     }
